8fab56d9f1c0659626870182fe5c53e9bac5b1f1
AliHassanL12 <liaqqahl@gmail.com>
2025-02-24 18:00:45 +0000

    Add code to retrieve simple weather data through API

--- processData.py
+++ processData.py
@@ -1,1 +1,18 @@
-import xarray as xr
+# source myenv/bin/activate & deactivate
+
+import cdsapi
+
+client = cdsapi.Client()
+client.retrieve("reanalysis-era5-land",
+    {
+        "variable": "2m_temperature",
+        "year": "2024",
+        "month": "01",
+        "day": "01",
+        "time": "12:00",
+        "format": "netcdf"
+    },
+    "test.nc"
+)
+
+print("Download complete!")
