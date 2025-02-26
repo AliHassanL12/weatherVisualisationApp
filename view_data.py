84877369cc4a24747f7dcbe59f5678111807af87
AliHassanL12 <liaqqahl@gmail.com>
2025-02-26 22:31:50 +0000

    Open official dataset and select subset
    Loading the entire dataset may be computationally expensive, in which
    case we take a specific subset of the year 2001 and select that. We
    then also converted it so that it is easier for parsing by the browser
    when we send it to the front-end for visualisation.

--- view_data.py
+++ view_data.py
@@ -3,14 +3,19 @@ import pprint
 import xarray as xr
 import matplotlib.pyplot as plt
 
-data = xr.open_dataset('download.grib', engine='cfgrib')
+ds = xr.open_dataset('./downloads/ERA5_L1_2001.nc')
 
-z_data = data['z']
+# Select a subset by selecting a specific time and pressure level
 
-plt.figure(figsize=(10, 6))
-plt.contourf(data['longitude'], data['latitude'], z_data, cmap='viridis')
-plt.colorbar(label='Geopotential')
-plt.xlabel('Longitude')
-plt.ylabel('Latitude')
-plt.title('Geopotential')
-plt.show()
+subset = ds.sel(time=slice('2001-01-01', '2001-03-30'))
+
+'''
+Front-end requires data in a serializable format. The netCDF file we have loaded,
+and the subset of it that we have chosen contains multidimensional arrays and 
+coordinates, which are not able to be directly translated to JSON format. 
+
+Hence we convert the object to a dictionary format, and then serialise that into JSON
+and set it over HTTP to the front-end, for parsing and handling.
+'''
+
+subset_dict = subset.to_dict()
