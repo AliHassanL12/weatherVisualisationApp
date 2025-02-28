8dc97243977ef54b08fd1f6c05c9814033787fed
AliHassanL12 <liaqqahl@gmail.com>
2025-02-28 15:20:37 +0000

    Convert variable dictionaries to serializable format and group

--- view_data.py
+++ view_data.py
@@ -11,6 +11,8 @@ subset = ds.sel(
     longitude=slice(10, 20)   
 )
 
+print(subset)
+
 '''
 Front-end requires data in a serializable format. The netCDF file we have loaded,
 and the subset of it that we have chosen contains multidimensional arrays and 
@@ -19,10 +21,38 @@ coordinates, which are not able to be directly translated to JSON format.
 Hence we convert the object to a dictionary format, and then serialise that into JSON
 and send it over HTTP to the front-end, for parsing and handling.
 '''
+
+
+# Extract individual variables of interest from subset dataset 
+
 t2mSubset = subset['t2m']
-e_subset = subset['e']
+eSubset = subset['e']
+d2mSubset = subset['d2m']
+tccSubset = subset['tcc']
+tpSubset = subset['tp']
+tcwvSubset = subset['tcwv']
 subsetDict = subset.to_dict()
 
-print(t2mSubset)
-print(e_subset)
+
+# Convert to a serializable format
+
+t2mDict = t2mSubset.to_dict()
+eDict = eSubset.to_dict()
+d2mDict = d2mSubset.to_dict()
+tccDict = tccSubset.to_dict()
+tpDict = tpSubset.to_dict()
+tcwvDict = tcwvSubset.to_dict()
+
+# Combine into one dictionary
+
+dataDict = {
+    't2m': t2mDict,
+    'e': eDict,
+    'd2m': d2mDict,
+    'tcc': tccDict,
+    'tp': tpDict,
+    'tcwv': tcwvDict
+}
+
+
 
