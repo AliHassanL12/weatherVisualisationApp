015c40c1ed2eed91365882bc67f92629473f399b
AliHassanL12 <liaqqahl@gmail.com>
2025-02-28 01:11:01 +0000

    Select specific variables in subset
    One of the largest issues we are going to likely run into is the
    one that results in us sending over a JSON object that is huge due
    to the data in the subset dataset. This can cause serious issues
    like unresponsive visualisations later, so we want to try and select
    variables that we are interested in, along a specific time slice,
    along a specific longitude and latitude. Then, the idea is that
    we convert into a JSON object, use an API to send the dataset to
    the front-end, and then use three.js to visualise it. The general
    overview is simple, however, actual implementation may prove to be
    complex.

--- view_data.py
+++ view_data.py
@@ -1,5 +1,3 @@
-import cfgrib
-import pprint
 import xarray as xr
 import matplotlib.pyplot as plt
 
@@ -7,7 +5,11 @@ ds = xr.open_dataset('./downloads/ERA5_L1_2001.nc')
 
 # Select a subset by selecting a specific time and pressure level
 
-subset = ds.sel(time=slice('2001-01-01', '2001-03-30'))
+subset = ds.sel(
+    time=slice('2001-01-01', '2001-03-30'),
+    latitude=slice(40, 50),   
+    longitude=slice(10, 20)   
+)
 
 '''
 Front-end requires data in a serializable format. The netCDF file we have loaded,
@@ -15,7 +17,12 @@ and the subset of it that we have chosen contains multidimensional arrays and
 coordinates, which are not able to be directly translated to JSON format. 
 
 Hence we convert the object to a dictionary format, and then serialise that into JSON
-and set it over HTTP to the front-end, for parsing and handling.
+and send it over HTTP to the front-end, for parsing and handling.
 '''
+t2mSubset = subset['t2m']
+e_subset = subset['e']
+subsetDict = subset.to_dict()
 
-subset_dict = subset.to_dict()
+print(t2mSubset)
+print(e_subset)
+
