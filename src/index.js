3ed21fc4ca0f374815e2c4ea7897e934c66c5539
AliHassanL12 <liaqqahl@gmail.com>
2025-03-04 20:56:05 +0000

    Change border size to border box
    I forget what the default it, but border box is far less incosistent
    when calculating the size of a box. Now we can rest assured that
    any size, padding, margin we set to a box will be exactly reflected
    in our application.

--- src/index.js
+++ src/index.js
@@ -7,3 +7,16 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const geometry = new THREE.SphereGeometry(5, 32, 32);
+const material = new THREE.MeshBasicMaterial({ color: 0x0077ff, wireframe: true });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+camera.position.z = 10;
+
+function animate() {
+    requestAnimationFrame(animate);
+    sphere.rotation.y += 0.01; 
+    renderer.render(scene, camera);
+}
+animate();
