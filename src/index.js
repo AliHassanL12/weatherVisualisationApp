26b4bbb1dfeb8c178b80338141edf600b8290786
AliHassanL12 <liaqqahl@gmail.com>
2025-03-09 02:00:51 +0000

    Change sphereGeometry to IcosahedronGeometry
    This is done because icosahedronGeometry is made up of equilateral
    triangles that are evenly distributed across the surface, whereas
    sphereGeometry uses rectangles, so it is a bit less uniform. We've
    actually seen before, in one of the commits that sometimes when we
    apply a texture to sphereGeomtry it can get stretched in certain
    areas. We care about distortion, so IcosahedronGeometry is the be-
    tter choice.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 import * as THREE from 'three';
-import earthImage from './images/earth.png';
+import earthImage from './images/earthmap1k.jpg';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import '../css/styles.css';
+const w = window.innerWidth;
+const h = window.innerHeight;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+camera.position.z = 10;
+const renderer = new THREE.WebGLRenderer({antialias: true});
 
 // set canvas size to full screen size 
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
+new OrbitControls(camera, renderer.domElement);
+
 // create spehere and add to the scene
-const geometry = new THREE.SphereGeometry(5, 32, 32);
+const geometry = new THREE.IcosahedronGeometry(3, 14);
 const textureLoader = new THREE.TextureLoader();
 const earthTexture = textureLoader.load(earthImage);
 
-const material = new THREE.MeshBasicMaterial({ map: earthTexture });
+
+const material = new THREE.MeshStandardMaterial({ 
+    map: earthTexture,
+    flatshading: true, 
+});
   
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+//Set direction from which light is coming
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
-// set camerate position
-camera.position.z = 10;
+
 
 // animate the sphere rotating
 function animate() {
     requestAnimationFrame(animate);
-    sphere.rotation.y += 0.01; 
+    sphere.rotation.y += 0.001; 
     renderer.render(scene, camera);
 }
 animate();
