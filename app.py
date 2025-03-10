07d483cf23ea4ed32cf863bf2d7ac75fa8e018b3
AliHassanL12 <liaqqahl@gmail.com>
2025-03-10 03:12:14 +0000

    Install CORS and implement in app.py
    This allows cross origin resource sharing, which will be important
    in getting around the same origin policy if the front end is serv-
    ed on a different origin from that of Flask app.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from view_data import getWeatherData  # Import from views.py
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 # Register route from views.py (this is the URL path that my flask application will listen to)
 app.add_url_rule('/getWeatherData', 'getWeatherData', getWeatherData)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
