b23bd597ee66f892cdfa031b47cfc3e99cf0c652
AliHassanL12 <liaqqahl@gmail.com>
2025-03-11 14:27:26 +0000

    Move camera closer in order for earth to appear bigger

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 10;
+camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({antialias: true});
 
 // set canvas size to full screen size 
