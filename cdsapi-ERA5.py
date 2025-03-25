df82b7e46f6127cfc22386c18226b40aa2fc9a98
AliHassanL12 <liaqqahl@gmail.com>
2025-03-25 11:57:16 +0000

    PULL CDSAPI ERA5 pressure level dataset and download

---
+++ cdsapi-ERA5.py
@@ -0,0 +1,40 @@
+import cdsapi
+
+dataset = "reanalysis-era5-pressure-levels-monthly-means"
+request = {
+    "product_type": ["monthly_averaged_reanalysis"],
+    "variable": [
+        "geopotential",
+        "relative_humidity",
+        "specific_cloud_ice_water_content",
+        "specific_cloud_liquid_water_content",
+        "temperature"
+    ],
+    "pressure_level": [
+        "1", "2", "3",
+        "5", "7", "10",
+        "20", "30", "50",
+        "70", "100", "125",
+        "150", "175", "200",
+        "225", "250", "300",
+        "350", "400", "450",
+        "500", "550", "600",
+        "650", "700", "750",
+        "775", "800", "825",
+        "850", "875", "900",
+        "925", "950", "975",
+        "1000"
+    ],
+    "year": ["2020"],
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "time": ["00:00"],
+    "format": "netcdf"
+}
+
+client = cdsapi.Client()
+client.retrieve(dataset, request).download("era5_cloud_structure_2020.nc")
---
+++ downloads/era5_temperature.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d62a4b89fd9031d30d4470627c47887caec7186fd16bb1e990e69301e690a313
+size 522403044
