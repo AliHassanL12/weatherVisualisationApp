23e3f19a4df1c7091575a7a57c52b59d621a884f
AliHassanL12 <liaqqahl@gmail.com>
2025-03-26 17:21:27 +0000

    Create 3D texture from back-end array and shape data
    This is needed for raymarching. We remove the old instanced Mesh
    plotting, which is no longer needed since it stresses the CPU, and
    move to a GPU-based 3D volumetric rendering.

--- src/fetch-request.js
+++ src/fetch-request.js
@@ -1,42 +1,20 @@
 
-export async function fetchWeatherData() {
+async function fetchRawCloudData() {
     try {
         const response = await fetch('http://127.0.0.1:5001/getWeatherData');
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         };
-        const data = await response.json();
-        return data;
+        const json = await response.json();
+        return json;
     } catch (error) {
         console.error('Problem fetching weather data', error);
         return null;
     };
 };
 
-
-/** 
- * we need to convert our latitude and longitude values to 3D cartesian coordinates 
- * The Formulas are as follows:
- * x = R * cos(lat) * cos(lon)
- * y = R * cos(lat) * sin(lon)
- * z = R *sin(lat)
- * 
- * Where R is the radius of the earth. Note this assumes the earth is a sphere rather than
- * an ellipsoid, which works in our case given that it is the former. 
- * */ 
-
-export function convertToCartesian(lat, lon, radius) {
-    const latRad = (lat * Math.PI) / 180; // Convert to radians (our function expect them in this format)
-    const lonRad = (lon * Math.PI) / 180; // Convert to radians
-
-
-    const x = radius * Math.cos(latRad) * Math.cos(lonRad);
-    const y = radius * Math.cos(latRad) * Math.sin(lonRad);
-    const z = radius * Math.sin(latRad);
-
-    return { x, y, z }; // returning as an object
+export {
+    fetchRawCloudData
 }
-
-
     
 
---
+++ src/texture-utils.js
@@ -0,0 +1,20 @@
+import * as THREE from 'three';
+
+function create3DTextureFromData(data,shape) {
+    const textureArray = new Float32Array(data);
+    // essentially extracting width, height and depth from shape
+    const texture = new THREE.Data3DTexture(textureArray, shape[2], shape[1], shape[0])
+
+    texture.format = THREE.RedFormat;
+    texture.type = THREE.FloatType;
+    texture.minFilter = THREE.LinearFilter;
+    texture.magFilter = THREE.LinearFilter;
+    texture.unpackAlignment = 1;
+    texture.needsUpdate = true;
+    
+    return texture;
+}
+
+export {
+    create3DTextureFromData
+}
