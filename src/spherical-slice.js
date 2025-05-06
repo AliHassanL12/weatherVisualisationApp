fc71bbc672e0418ef8fece95fb786a1e1f799f67
AliHassanL12 <liaqqahl@gmail.com>
2025-05-06 18:57:09 +0100

    Create temperature pressure slice

--- src/spherical-slice.js
+++ src/spherical-slice.js
@@ -48,4 +48,58 @@ function createSphericalSlice(data3DTexure, textureShape, initialPressureIndex =
     return { sphere, material};
 }
 
-export {createSphericalSlice}
+function createTempSlice() {
+    const sliceRadius = 4.0;
+    const geometry = new THREE.SphereGeometry(sliceRadius, 128, 128);
+    const material = new THREE.ShaderMaterial({
+        transparent: true,
+        uniforms: {
+            uTexture3D: {value: data3DTexure},
+            uPressureIndex: { value: initialPressureIndex},
+            uTextureShape: { value: new THREE.Vector3(...textureShape)},
+        },
+        vertexShader:`
+        varying vec3 vPosition;
+        void main() {
+            vPosition = normalize(position);
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+        `,
+        fragmentShader: `
+        precision highp float;
+        uniform sampler3D uTexture3D;
+        uniform float uPressureIndex;
+        uniform vec3 uTextureShape;
+        varying vec3 vPosition;
+        
+        vec3 temperatureToColor(float t) {
+            t = clamp((t - 240.0) / 60.0, 0.0, 1.0);
+            return mix(vec3(0.0, 0.2, 1.0), vec3(1.0, 0.0, 0.0), t);
+        }
+
+
+        void main() {
+            float lat = asin(vPosition.y);
+            float lon = atan(vPosition.z, vPosition.x);
+            float u = (lon + 3.1415926) / (2.0 * 3.1415926);
+            float v = (lat + 3.1415926 / 2.0) / 3.1415926;
+            float x = u;
+            float y = v;
+            float z = uPressureIndex / (uTextureShape.x - 1.0);
+            vec3 texCoord = vec3(z, y, x);
+            float value = texture(uTexture3D, texCoord).r;
+            vec3 color = temperatureToColor(value);
+            gl_FragColor = vec4(color, 0.6);
+            }
+        `,
+        side: THREE.DoubleSide,
+        depthWrite: false,
+        blending: THREE.NormalBlending,
+    });
+
+    const sphere = new THREE.Mesh(geometry, material);
+    sphere.name = 'SphericalTempSlice';
+    return { sphere, material};
+
+}
+export {createSphericalSlice, createTempSlice};
